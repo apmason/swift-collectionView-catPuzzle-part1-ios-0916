6145ccbcd695a4e4abcb17ceebae0e2206ef3cc7
Alex Mason <apmason119@gmail.com>
2016-10-12 08:58:25 -0400

    Complete

---
+++ CollectionViewCatPicPuzzle/CollectionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CollectionViewController.swift
+//  CollectionViewCatPicPuzzle
+//
+//  Created by Alexander Mason on 10/11/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UICollectionViewController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "puzzleCell", for: indexPath) as! CollectionViewCell
+        cell.imageView.image = UIImage(named: "cats")
+        return cell
+    }
+}
